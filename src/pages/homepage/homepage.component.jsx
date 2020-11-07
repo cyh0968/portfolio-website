d90e9bd0537592dc1713a9315ea629d5f4f7e319
Yohan Choi <ychoi63@myseneca.ca>
2020-11-06 20:43:58 -0500

    creating reaat-app and adding html css templates files for the next react project

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import './homepage.styles.scss';
+
+const Homepage = (props) => (
+    <div className='homepage'>
+        <header>
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+
+        </header>
+
+    </div>)
