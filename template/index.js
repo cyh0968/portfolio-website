14459fb53f942fc1bbe04380596f13e172db789c
Yohan Choi <ychoi63@myseneca.ca>
2020-11-13 01:44:13 -0500

    updating template script file to clean code base

--- template/index.js
+++ template/index.js
@@ -1,12 +1,5 @@
 const navigationToggle = document.getElementById('navigation-toggle');
 const navigationToggleMenu = document.getElementById('navigation-toggle-menu');
-
-const uncheckCheckbox = () => {
-    navigationToggle.checked = false;
-}
-
-window.onload = (event) => {
-    console.log('successfully loaded a page!');
-    navigationToggleMenu.addEventListener('click', uncheckCheckbox);
-};
+const uncheckCheckbox = () => navigationToggle.checked = false;
+navigationToggleMenu.addEventListener('click', uncheckCheckbox);
 
