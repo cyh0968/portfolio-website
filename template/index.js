ba71157196072fb7d3e5dd6f6f4bfcf826fb29b6
Yohan Choi <ychoi63@myseneca.ca>
2020-11-12 22:58:40 -0500

    updating tememplate stylesheet to prevent text selection

--- template/index.js
+++ template/index.js
@@ -1,16 +1,8 @@
 const navigationToggle = document.getElementById('navigation-toggle');
 const navigationToggleMenu = document.getElementById('navigation-toggle-menu');
-const defaultDisplaySetting = navigationToggleMenu.style.display;
+
 const uncheckCheckbox = () => {
     navigationToggle.checked = false;
-}
-const hideNavigationToggleMenu = () => {
-    if (navigationToggle.checked) {
-        navigationToggle.style.display = 'none';
-        uncheckCheckbox();
-    } else {
-        navigationToggle.style.display = defaultDisplaySetting;
-    }
 }
 
 window.onload = (event) => {
