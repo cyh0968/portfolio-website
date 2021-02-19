97a723058606bf3f09bd29f016872b298d6745df
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-18 22:00:36 -0500

    fix: added paragraph styles to util styles and removed paragraph styles from global style

--- pages/homepage/homepage.component.jsx
+++ pages/homepage/homepage.component.jsx
@@ -9,13 +9,13 @@ function Homepage() {
             <section className="section" id="about">
                 <div className="section__content">
                     <h1 className={utilStyles.heading2Xl}>About</h1>
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <p>Hello, I'm Yohan. Thank you for visiting my website!</p>
-                        <p className="long-copy">
+                        <p className={utilStyles.long}>
                             I believe that our fingertips can make a better world. As a software developer, I wish my
                             code is working in the real world and contributing to our society and community.
                         </p>
-                        <p className="long-copy">
+                        <p className={utilStyles.long}>
                             To create a better program, I do not hesitate to refactor my code. Attitude to find the best
                             solution for a problem is my asset. The intense passion for programming made me start to
                             join an open-source. Self-learning with a passion for programming is a nutrient to be a
@@ -25,26 +25,26 @@ function Homepage() {
                 </div>
                 <div className="section__content">
                     <h2 className={utilStyles.headingXl}>Skills</h2>
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <h3 className={utilStyles.headingLg}>Programming Languages</h3>
                         <h4 className={utilStyles.headingMd}>
                             JavaScript, Typescript, Java, C, C++, C#, Python, Bash, Powershell
                         </h4>
                     </div>
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <h3 className={utilStyles.headingLg}>Database</h3>
                         <h4 className={utilStyles.headingMd}>
                             IBM DB2, Oracle Database, PostgreSQL, MySQL, SQLite, MongoDB, Redis
                         </h4>
                     </div>
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <h3 className={utilStyles.headingLg}>Frameworks / Libraries</h3>
                         <h4 className={utilStyles.headingMd}>
                             Node.js/express, React.js, Redux, SASS, Babel, Webpack, Jest, jQuery, Socket.io, Sequelize,
                             Mongoose, ASP.net,Java EE, JDBC
                         </h4>
                     </div>
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <h3 className={utilStyles.headingLg}>OS / Environments</h3>
                         <h4 className={utilStyles.headingMd}>
                             Ubuntu, Windows 7, Windows 10, Windows Server 2012, Digital Ocean Droplet, AWS EC2, AWS EBS,
@@ -52,7 +52,7 @@ function Homepage() {
                         </h4>
                     </div>
 
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <h3 className={utilStyles.headingLg}>Tools</h3>
                         <h4 className={utilStyles.headingMd}>
                             Nginx, Visual Studio, VSCode, IntelliJ, Eclipse, Figma, Postman, Visio, Trello
@@ -61,45 +61,47 @@ function Homepage() {
                 </div>
                 <div className="section__content">
                     <h2 className={utilStyles.headingXl}>Experience</h2>
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <h3 className={utilStyles.headingLg}>Software Developer</h3>
                         <h4 className={utilStyles.headingMd}>Emerson DEC 2019 - SEP 2020</h4>
-                        <span className="details">details</span>
-                        <div className="paragraph">
+                        <span className={utilStyles.details}>details</span>
+                        <div className={utilStyles.paragraph}>
                             <ul>
-                                <li className="long-copy">
+                                <li className={utilStyles.long}>
                                     Developed features for intranet user assistance application using React.js,
                                     Electron, SASS
                                 </li>
-                                <li className="long-copy">
+                                <li className={utilStyles.long}>
                                     Developed Node.js automating test tools for database migration application
                                 </li>
-                                <li className="long-copy">
+                                <li className={utilStyles.long}>
                                     Developed DB2/Oracle Database Migration Application using JDBC
                                 </li>
-                                <li className="long-copy">
+                                <li className={utilStyles.long}>
                                     Contributed to development and maintenance of Warehouse Managemenet System using
                                     Java EE
                                 </li>
-                                <li className="long-copy">
+                                <li className={utilStyles.long}>
                                     Developed a Powershell automation script to update user applications, which it
                                     increased software update rate by 20%
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <h3 className={utilStyles.headingLg}>ITS Technologist</h3>
                         <h4 className={utilStyles.headingMd}>Seneca College Apr 2019 - Aug 2019</h4>
-                        <span className="details">details</span>
-                        <div className="paragraph">
+                        <span className={utilStyles.details}>details</span>
+                        <div className={utilStyles.paragraph}>
                             <ul>
-                                <li className="long-copy">Updated and reported issues on the helpdesk website</li>
-                                <li className="long-copy">
+                                <li className={utilStyles.long}>Updated and reported issues on the helpdesk website</li>
+                                <li className={utilStyles.long}>
                                     Assisted field service through client support, assigned task and research
                                 </li>
-                                <li className="long-copy">Provided user support service to resolve technical issues</li>
-                                <li className="long-copy">
+                                <li className={utilStyles.long}>
+                                    Provided user support service to resolve technical issues
+                                </li>
+                                <li className={utilStyles.long}>
                                     Installed and maintained equipment of E-classNameroom and computer labs
                                 </li>
                             </ul>
@@ -108,7 +110,7 @@ function Homepage() {
                 </div>
                 <div className="section__content">
                     <h2 className={utilStyles.headingXl}>Education</h2>
-                    <div className="text-container">
+                    <div className={utilStyles.content}>
                         <h3 className={utilStyles.headingLg}>Computer Programming and Analysis</h3>
                         <h4 className={utilStyles.headingMd}>Seneca College JAN 2018 - DEC 2020</h4>
                     </div>
@@ -118,9 +120,9 @@ function Homepage() {
                 <div className="section__content">
                     <h1 className={utilStyles.heading2Xl}>Project</h1>
                     <h2 className={utilStyles.headingXl}>Fleamarket</h2>
-                    <div className="text-container">
-                        <span className="details">Description</span>
-                        <div className="paragraph">
+                    <div className={utilStyles.content}>
+                        <span className={utilStyles.details}>Description</span>
+                        <div className={utilStyles.paragraph}>
                             Fleamarket is a real-time trading application to help users to exchange their property in a
                             fast and easy way The project is a web application which is developed in modern JavaScript
                             technologies such as node.js/express.js and react.js by using Nginx reverse proxy to serve
@@ -138,9 +140,9 @@ function Homepage() {
                         </p>
                     </div>
                     <h2 className={utilStyles.headingXl}>Employee Management System</h2>
-                    <div className="text-container">
-                        <span className="details">Description</span>
-                        <div className="paragraph">
+                    <div className={utilStyles.content}>
+                        <span className={utilStyles.details}>Description</span>
+                        <div className={utilStyles.paragraph}>
                             The EMS application helps a user to manage employees on the web The project is developed in
                             server-side JavaScript technologies such as node.js/express.js, MongoDB, and PostgreSQL
                         </div>
