61d318bee205d687756c4551e48a5bcacd78d07b
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-19 02:52:47 -0500

    feature: added blog page

---
+++ pages/blog/index.jsx
@@ -0,0 +1,11 @@
+import { Layout } from '../../components/layout';
+
+function Blog() {
+    return (
+        <Layout>
+            <h2 className="headingXl">Blog</h2>
+        </Layout>
+    );
+}
+
+export default Blog;
