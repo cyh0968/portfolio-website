7e85e124eca763dbd04cdff61cdf587ae8c0b030
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-19 02:58:57 -0500

    refactor: updated icon class name

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -17,30 +17,30 @@ function Contact() {
                                     target="_blank"
                                     className={styles.link}
                                 >
-                                    <LinkedInIcon name="logo-linkedin" className={utilStyles.iconLg}></LinkedInIcon>
+                                    <LinkedInIcon name="logo-linkedin" className={utilStyles.iconMd}></LinkedInIcon>
                                     Linkedin
                                 </a>
                             </li>
                             <li className={styles.list}>
                                 <a href="https://github.com/yohan-choi-dev" target="_blank" className={styles.link}>
-                                    <GithubIcon name="logo-github" className={utilStyles.iconLg}></GithubIcon>GITHUB
+                                    <GithubIcon name="logo-github" className={utilStyles.iconMd}></GithubIcon>GITHUB
                                 </a>
                             </li>
 
                             <li className={styles.list}>
                                 <a href="https://twitter.com/remember2019" target="_blank" className={styles.link}>
-                                    <TwitterIcon name="logo-twitter" className={utilStyles.iconLg}></TwitterIcon>TWITTER
+                                    <TwitterIcon name="logo-twitter" className={utilStyles.iconMd}></TwitterIcon>TWITTER
                                 </a>
                             </li>
                             <li className={styles.list}>
                                 <a href="malito:yohan.choi.dev@gmail.com" target="_blank" className={styles.link}>
-                                    <MailIcon name="mail-outline" className={utilStyles.iconLg}></MailIcon>EMAIL
+                                    <MailIcon name="mail-outline" className={utilStyles.iconMd}></MailIcon>EMAIL
                                 </a>
                             </li>
 
                             <li className={styles.list}>
                                 <a href="https://tinyurl.com/y4yncva7" target="_blank" className={styles.link}>
-                                    <ResumeIcon name="document-outline" className={utilStyles.iconLg}></ResumeIcon>
+                                    <ResumeIcon name="document-outline" className={utilStyles.iconMd}></ResumeIcon>
                                     RESUME
                                 </a>
                             </li>
