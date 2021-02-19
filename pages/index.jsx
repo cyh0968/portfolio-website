c3ee94967c6e297198e23946c7d00aedfa2af53d
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-19 10:28:44 -0500

    feature: added intro animation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './index.module.scss';
 import utilStyles from '../styles/utils.module.scss';
 import { Layout } from '../components/layout';
@@ -6,22 +7,25 @@ function Homepage() {
     return (
         <Layout>
             <div className={styles.container}>
-                <h1 className={utilStyles.heading2Xl}>Welcome to Yohan's website!</h1>
-
-                <ul>
-                    <li>About</li>
-                    <li>Project</li>
-                    <li>Contact</li>
-                    <li>
-                        Blog
-                        <ul>
-                            <li>Post 1</li>
-                            <li>Post 2</li>
-                            <li>Post 3</li>
-                            <li>post 4</li>
-                        </ul>
-                    </li>
-                </ul>
+                <div className={styles.intro}>
+                    <span className={styles.leftAlign}>
+                        <h1 className={styles.introText}>Full-stack web</h1>
+                        <h1 className={styles.introText}>developer</h1>
+                    </span>
+                    <span className={styles.rightAlign}>
+                        <h1 className={styles.introText}>currently based in</h1>
+                        <h1 className={styles.introText}>Toronto.</h1>
+                    </span>
+                    <span className={styles.leftAlign}>
+                        <h1 className={styles.introText}>Focussed on</h1>
+                        <h1 className={styles.introText}>JavaScript</h1>
+                        <h1 className={styles.introText}>Technologies</h1>
+                    </span>
+                    <span className={styles.rightAlign}>
+                        <h1 className={styles.introText}>Long journey in</h1>
+                        <h1 className={styles.introText}>craftmanship</h1>
+                    </span>
+                </div>
             </div>
         </Layout>
     );
