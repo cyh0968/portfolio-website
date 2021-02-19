3a5ab0217ce1a8f2137844bf429d7ba2fe34c1c7
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-18 22:36:48 -0500

    updated heading styles

--- components/layout/header/header.component.jsx
+++ components/layout/header/header.component.jsx
@@ -1,14 +1,13 @@
 import styles from './header.module.scss';
+import utilStyles from '../../../styles/utils.module.scss';
 import { Navigation } from '../navigation';
 
 function Header() {
     return (
         <div>
-            <header class="header" id="home">
+            <header className={styles.header} id="home">
                 <Navigation></Navigation>
-                <div class="header__textbox">
-                    <h1>Thank you for visiting</h1>
-                </div>
+                <h1 className={utilStyles.heading2Xl}>Thank you for visiting</h1>
             </header>
         </div>
     );
