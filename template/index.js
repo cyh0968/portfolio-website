e17ef8efba9c943490d05923ac481e3ed1f73695
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-18 20:20:39 -0500

    removed html files from the project foldere

--- template/index.js
+++
@@ -1,5 +0,0 @@
-const navigationToggle = document.getElementById('navigation-toggle');
-const navigationToggleMenu = document.getElementById('navigation-toggle-menu');
-const uncheckCheckbox = () => navigationToggle.checked = false;
-navigationToggleMenu.addEventListener('click', uncheckCheckbox);
-
