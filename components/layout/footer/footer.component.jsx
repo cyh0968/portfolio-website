cad40f64f15cd71d403dc2ad691853664513548c
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-18 22:09:41 -0500

    updated footer styles

--- components/layout/footer/footer.component.jsx
+++ components/layout/footer/footer.component.jsx
@@ -1,12 +1,11 @@
 import styles from './footer.module.scss';
+import utilStyles from '../../../styles/utils.module.scss';
 function Footer() {
     return (
         <footer>
-            <div className="footer__container">
-                <div className="footer__text">
-                    <p>
-                        <span className="small">&copy; 2020 by Yohan Choi. All rights reserved.</span>
-                    </p>
+            <div className={styles.footerContainer}>
+                <div className={styles.footerText}>
+                    <p className={utilStyles.small}>&copy; 2020 by Yohan Choi. All rights reserved.</p>
                 </div>
             </div>
         </footer>
