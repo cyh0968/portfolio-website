34338dc6385c0771081f72b77f72c348298aa3a3
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-19 10:28:17 -0500

    fix: updated minor styles

--- components/ui-components/icons/index.js
+++ components/ui-components/icons/index.js
@@ -3,5 +3,5 @@ export { IoLogoGithub as GithubIcon } from 'react-icons/io5';
 export { IoLogoTwitter as TwitterIcon } from 'react-icons/io5';
 export { IoMailOutline as MailIcon } from 'react-icons/io5';
 export { IoDocumentOutline as ResumeIcon } from 'react-icons/io5';
-export { IoChevronDown as ChevronDown } from 'react-icons/io5';
-export { IoChevronUp as ChevronUp } from 'react-icons/io5';
+export { IoChevronDownSharp as ChevronDown } from 'react-icons/io5';
+export { IoChevronUpSharp as ChevronUp } from 'react-icons/io5';
