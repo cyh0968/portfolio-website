9aa9091f6a23e5c0a156d064b966ace77d5c8d33
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-22 20:23:09 -0500

    changed footer contact item transition effects

--- components/layout/footer/footer.component.jsx
+++ components/layout/footer/footer.component.jsx
@@ -4,9 +4,9 @@ import { LinkedInIcon, GithubIcon, TwitterIcon, MailIcon } from '../../ui-compon
 
 function Footer() {
     return (
-        <div className={styles.footerContainer}>
-            <footer>
-                <div className={styles.footerContact} id="contact">
+        <footer className={styles.footer}>
+            <div className={styles.contact} id="contact">
+                <span className={styles.item}>
                     <a
                         href="https://www.linkedin.com/in/yohan-choi-dev/"
                         target="_blank"
@@ -15,6 +15,8 @@ function Footer() {
                     >
                         <LinkedInIcon name="logo-linkedin" className={utilStyles.iconMd}></LinkedInIcon>
                     </a>
+                </span>
+                <span className={styles.item}>
                     <a
                         href="https://github.com/yohan-choi-dev"
                         target="_blank"
@@ -23,6 +25,8 @@ function Footer() {
                     >
                         <GithubIcon name="logo-github" className={utilStyles.iconMd}></GithubIcon>
                     </a>
+                </span>
+                <span className={styles.item}>
                     <a
                         href="https://twitter.com/remember2019"
                         target="_blank"
@@ -31,15 +35,17 @@ function Footer() {
                     >
                         <TwitterIcon name="logo-twitter" className={utilStyles.iconMd}></TwitterIcon>
                     </a>
+                </span>
+                <span className={styles.item}>
                     <a href="malito:yohan.choi.dev@gmail.com" target="_blank" className={utilStyles.link}>
                         <MailIcon name="mail-outline" className={utilStyles.iconMd} title="Mail"></MailIcon>
                     </a>
-                </div>
-                <div className={styles.footerText}>
-                    <p className={utilStyles.small}>&copy; 2020 by Yohan Choi. All rights reserved.</p>
-                </div>
-            </footer>
-        </div>
+                </span>
+            </div>
+            <div className={styles.copyright}>
+                <p className={utilStyles.small}>&copy; 2020 by Yohan Choi. All rights reserved.</p>
+            </div>
+        </footer>
     );
 }
 
