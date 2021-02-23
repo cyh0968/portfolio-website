78981ae94fd339689be8e5ce5225461ca89b8202
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-22 23:49:54 -0500

    added logo animation

--- components/ui-components/logos/logo-container/logo-container.component.jsx
+++ components/ui-components/logos/logo-container/logo-container.component.jsx
@@ -3,8 +3,10 @@ import styles from './logo-container.module.scss';
 function LogoContainer({ name, children }) {
     return (
         <div className={styles.container} name={name}>
-            <div className={styles.logo}>{children}</div>
-            <div className={styles.text}>{name}</div>
+            <div className={styles.card}>
+                <div className={styles.logo}>{children}</div>
+                <div className={styles.text}>{name}</div>
+            </div>
         </div>
     );
 }
