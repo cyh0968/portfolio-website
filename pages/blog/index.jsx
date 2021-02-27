3414f48b69a67cf41d231e9103fdcf4c5ebbe177
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-27 00:04:33 -0500

    moved blog section

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,27 @@
+import utilStyles from '../../styles/utils.module.scss';
 import { Layout } from '../../components/layout';
+import { Section } from '../../components/section';
 
 function Blog() {
     return (
         <Layout>
-            <h2 className="headingXl">Blog</h2>
+            <Section leftHeading="Blog">
+                <div className={utilStyles.content}>
+                    <p className={utilStyles.long}>
+                        Thank you for visiting my website! I'm on the long journey of craftmanship. I'm a simple person
+                        who has only one purpose that is making good products.
+                    </p>
+                    <br />
+                    <p className={utilStyles.long}>
+                        I believe that the projects that I will work on will impact people's everyday lives, and I will
+                        be willing to take my responsibility to make the best.
+                    </p>
+                    <br />
+                    <p className={utilStyles.long}>
+                        A promise with myself is that I will remain humble and improve myself by the end of my journey.
+                    </p>
+                </div>
+            </Section>
         </Layout>
     );
 }
