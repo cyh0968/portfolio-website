cc9d177acf3967fbf89f6e468a01a856b9ee6110
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-28 17:33:02 -0500

    separated cubeface from cube component

---
+++ components/ui-components/face-list/face-list.component.jsx
@@ -0,0 +1,37 @@
+/* Utility */
+import PropType from 'prop-types';
+import { nanoid } from 'nanoid';
+import { mergeStrings } from '../../../utils';
+/* Style */
+import styles from './face-list.module.scss';
+const faceStyles = [
+    mergeStrings(styles.face, styles.front),
+    mergeStrings(styles.face, styles.right),
+    mergeStrings(styles.face, styles.back),
+    mergeStrings(styles.face, styles.left),
+    mergeStrings(styles.face, styles.top),
+    mergeStrings(styles.face, styles.bottom),
+];
+
+function* getFaceStyles() {
+    for (let faceStyle of faceStyles) {
+        yield faceStyle;
+    }
+}
+
+function FaceList({ children }) {
+    const faces = Array.isArray(children) ? children : [children];
+    const faceStyles = getFaceStyles();
+
+    return Array.from({ ...faces, length: 6 }).map((face) => (
+        <div key={nanoid()} className={faceStyles.next().value}>
+            {face}
+        </div>
+    ));
+}
+
+FaceList.propTypes = {
+    children: PropType.arrayOf(PropType.element),
+};
+
+export default FaceList;
