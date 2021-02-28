c9096450e4567d6507afa2e6b3af20262b91c2ba
Yohan Choi <yohan.choi.dev@gmail.com>
2021-02-28 17:25:58 -0500

    added utility function

---
+++ utils/mergeStrings.js
@@ -0,0 +1,20 @@
+export default function mergeStrings(options = { delimiter: ' ' }, ...arrayOfStrings) {
+    const isDelimiter = typeof options === 'object' && typeof options?.delimiter === 'string';
+    const delimiter = isDelimiter ? options.delimiter : ' ';
+    if (!isDelimiter && typeof options === 'string') {
+        arrayOfStrings.unshift(options);
+    }
+
+    if (!arrayOfStrings.length) {
+        throw new Error('no string passed!');
+    }
+
+    if (arrayOfStrings.length === 1) {
+        return arrayOfStrings[0];
+    }
+
+    return arrayOfStrings.reduce(
+        (mergedStyles, styles) => (typeof styles === 'string' ? mergedStyles + delimiter + styles : mergedStyles),
+        ''
+    );
+}
