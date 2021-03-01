ce5b2284274f729e39caec608ca662128f0914f1
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-01 16:58:27 -0500

    added typo animation component

---
+++ components/ui-components/typo-animation/typo-animation.component.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import PropType from 'prop-types';
+
+import styles from './typo-animation.module.scss';
+
+function TypoAnimation({ delay = 40, children = 'hello world' }) {
+    const [characters, setCharacter] = useState(' ');
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setCharacter((previous) =>
+                previous.length <= children.length ? previous + children[previous.length - 1] : previous
+            );
+        }, delay);
+        return () => clearTimeout(timeoutId);
+    }, [characters.length]);
+
+    return <>{characters}</>;
+}
+
+TypoAnimation.propTypes = {
+    children: PropType.string,
+};
+
+export default TypoAnimation;
