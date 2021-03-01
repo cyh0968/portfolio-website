947b21607286933cda112cf077661725db401073
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-01 01:19:45 -0500

    added animation effects

--- components/ui-components/slider/slider.component.jsx
+++ components/ui-components/slider/slider.component.jsx
@@ -1,44 +1,90 @@
+/* React */
 import { useState } from 'react';
-import { nanoid } from 'nanoid';
+
+/* Utility */
+import PropType from 'prop-types';
 import { mergeStrings } from '../../../utils';
 
+/* Styles */
 import styles from './slider.module.scss';
 
+const slideStatusStyles = {
+    current: mergeStrings(styles.slide, styles.current),
+    previous: mergeStrings(styles.slide, styles.previous),
+    next: mergeStrings(styles.slide, styles.next),
+    ready: mergeStrings(styles.slide, styles.ready),
+    waiting: mergeStrings(styles.slide, styles.waiting),
+};
+
 function Slider({ children }) {
-    const [slideIndex, setSliderIndex] = useState(0);
+    const [indexOfCurrentSlide, setIndexOfCurrentSlide] = useState(0);
     const slides = Array.isArray(children) ? children : [children];
-    function previousSlid() {
-        const length = slides.length;
-        slideIndex === 0 ? setSliderIndex(length - 1) : setSliderIndex((previous) => previous - 1);
+    const listSlides = slides.map((slide, index, array) => {
+        return (
+            <div key={index} className={getSlideStatusStyles(index, array.length)}>
+                {slide}
+            </div>
+        );
+    });
+
+    function getSlideStatusStyles(index, length) {
+        if (index === indexOfCurrentSlide) {
+            return slideStatusStyles.current;
+        }
+
+        if (index === indexOfCurrentSlide - 1) {
+            return slideStatusStyles.previous;
+        }
+
+        if (index === indexOfCurrentSlide + 1) {
+            return slideStatusStyles.next;
+        }
+
+        if (index === length - 1 && indexOfCurrentSlide === 0) {
+            return slideStatusStyles.previous;
+        }
+
+        if (index === 0 && indexOfCurrentSlide === length - 1) {
+            return slideStatusStyles.next;
+        }
+        const difference = Math.abs(index - indexOfCurrentSlide);
+
+        if (difference === 2 || difference === length - 2) {
+            return slideStatusStyles.ready;
+        }
+
+        return slideStatusStyles.waiting;
     }
 
-    function nextSlide() {
-        const length = slides.length;
-        slideIndex === length - 1 ? setSliderIndex(0) : setSliderIndex((previous) => previous + 1);
+    function showNextSlide() {
+        indexOfCurrentSlide < slides.length - 1
+            ? setIndexOfCurrentSlide((previous) => previous + 1)
+            : setIndexOfCurrentSlide(0);
     }
 
-    function setSlideVisibility(index) {
-        return slideIndex === index
-            ? mergeStrings(styles.slide, styles.visible)
-            : mergeStrings(styles.slide, styles.invisible);
+    function showPreviousSlide() {
+        indexOfCurrentSlide !== 0
+            ? setIndexOfCurrentSlide((previous) => previous - 1)
+            : setIndexOfCurrentSlide(slides.length - 1);
     }
-    const listSlides = slides.map((item, index) => (
-        <div key={nanoid()} className={setSlideVisibility(index)}>
-            {item}
-        </div>
-    ));
 
     return (
-        <div className={styles.slider}>
-            <a className={styles.previous} onClick={previousSlid}>
-                &#10094;
-            </a>
-            <a className={styles.next} onClick={nextSlide}>
-                &#10095;
-            </a>
-            {listSlides}
+        <div className={styles.container}>
+            <div className={styles.controller}>
+                <a className={styles.previousButton} onClick={showPreviousSlide}>
+                    &#10094;
+                </a>
+                <a className={styles.nextButton} onClick={showNextSlide}>
+                    &#10095;
+                </a>
+            </div>
+            <div className={styles.slider}>{listSlides}</div>
         </div>
     );
 }
+
+Slider.propTypes = {
+    children: PropType.arrayOf(PropType.element),
+};
 
 export default Slider;
