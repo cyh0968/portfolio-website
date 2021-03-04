b03b3184bdc37f77d0d683b89116ce7ce263ca8a
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-04 18:24:23 -0500

    added children to pass to the component

--- components/ui-components/ipad/ipad.component.jsx
+++ components/ui-components/ipad/ipad.component.jsx
@@ -1,26 +1,14 @@
 import styles from './ipad.module.scss';
 import IpadSvg from '../../../assets/devices/ipad.svg';
-import utilStyles from '../../../styles/utils.module.scss';
-
 import { TypoAnimation } from '../typo-animation';
 
-const text = `
-Thank you for visiting my website! I'm on the long journey of craftmanship. I'm a simple person
-who has only one purpose that is making good products.
-
-I believe that the projects that I will work on will impact people's everyday lives, and I will
-be willing to take my responsibility to make the best.
-
-A promise with myself is that I will remain humble and improve myself by the end of my journey.
-`;
-
-function Ipad() {
+function Ipad({ children }) {
     return (
         <div className={styles.container}>
             <IpadSvg className={styles.ipad}></IpadSvg>
             <div className={styles.screen}>
                 <code>
-                    <TypoAnimation>{text}</TypoAnimation>
+                    <TypoAnimation>{children}</TypoAnimation>
                 </code>
             </div>
         </div>
