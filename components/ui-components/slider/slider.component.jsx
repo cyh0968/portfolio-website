0f3f8b553cdea293c5690b7fa6e17f735c5c90b5
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-04 18:26:34 -0500

    refactored code and updated comments

--- components/ui-components/slider/slider.component.jsx
+++ components/ui-components/slider/slider.component.jsx
@@ -18,28 +18,59 @@ const slideStatusStyles = {
 
 function Slider({ width = '40rem', height = '20rem', autoPlayMode = false, playSpeed = 2000, children }) {
     const [indexOfCurrentSlide, setIndexOfCurrentSlide] = useState(0);
-    const slideSize = { width: width, height: height };
     const slides = Array.isArray(children) ? children : [children];
-    const listSlides = slides.map((slide, index, array) => (
-        <div
-            key={index}
-            className={getSlideStatusStyles(index, array.length)}
-            onClick={() => setIndexOfCurrentSlide(index)}
-        >
-            {slide}
-        </div>
-    ));
+    const slideSize = { width: width, height: height };
+
+    function showPreviousSlide() {
+        // Decrease the index of current slide by -1.
+        // If the current one is 0, the index will be the last index
+        indexOfCurrentSlide !== 0
+            ? setIndexOfCurrentSlide((previous) => previous - 1)
+            : setIndexOfCurrentSlide(slides.length - 1);
+    }
 
     function showNextSlide() {
+        // Increase the index of current slide by 1.
+        // If the current one is the last index, the index will be zero
         indexOfCurrentSlide < slides.length - 1
             ? setIndexOfCurrentSlide((previous) => previous + 1)
             : setIndexOfCurrentSlide(0);
     }
 
-    function showPreviousSlide() {
-        indexOfCurrentSlide !== 0
-            ? setIndexOfCurrentSlide((previous) => previous - 1)
-            : setIndexOfCurrentSlide(slides.length - 1);
+    function showSelectedSlide({ currentTarget: { id } }) {
+        setIndexOfCurrentSlide(parseInt(id));
+    }
+
+    function getSlideStatusStyles(index, length) {
+        /* 
+        Slide Style Priority
+        Current > left, right > ready > waiting
+        */
+        const difference = Math.abs(index - indexOfCurrentSlide);
+        // If the difference is zero, it is the current slide
+        if (difference === 0) {
+            return slideStatusStyles.current;
+        }
+
+        // If the difference is one, it is adjacent to the current slide
+        // If index < indexOfCurrent, it is the current's left, else right
+        if (difference === 1) {
+            return index < indexOfCurrentSlide ? slideStatusStyles.previous : slideStatusStyles.next;
+        }
+
+        //  If the difference is length - 1, the current slide locates on the first and the index is on the last
+        // Otherwise, the current one is the last and the index is the first one
+        // In those case, the slide of the index will be located to left of right of the current
+        if (difference === length - 1) {
+            return index > indexOfCurrentSlide ? slideStatusStyles.previous : slideStatusStyles.next;
+        }
+
+        // If the difference is two or length -2, it will be located on the ready position
+        if (difference === 2 || difference === length - 2) {
+            return slideStatusStyles.ready;
+        }
+
+        return slideStatusStyles.waiting;
     }
 
     useEffect(() => {
@@ -52,53 +83,29 @@ function Slider({ width = '40rem', height = '20rem', autoPlayMode = false, playS
         }
     }, []);
 
-    function getSlideStatusStyles(index, length) {
-        if (index === indexOfCurrentSlide) {
-            return slideStatusStyles.current;
-        }
-
-        if (index === indexOfCurrentSlide - 1) {
-            return slideStatusStyles.previous;
-        }
-
-        if (index === indexOfCurrentSlide + 1) {
-            return slideStatusStyles.next;
-        }
-
-        if (index === length - 1 && indexOfCurrentSlide === 0) {
-            return slideStatusStyles.previous;
-        }
-
-        if (index === 0 && indexOfCurrentSlide === length - 1) {
-            return slideStatusStyles.next;
-        }
-        const difference = Math.abs(index - indexOfCurrentSlide);
-
-        if (difference === 2 || difference === length - 2) {
-            return slideStatusStyles.ready;
-        }
-
-        return slideStatusStyles.waiting;
-    }
-
-    return autoPlayMode ? (
+    return (
         <div style={slideSize} className={styles.container}>
+            {!autoPlayMode && (
+                <div className={styles.controller}>
+                    <a className={styles.previousButton} onClick={showPreviousSlide}>
+                        &#10094;
+                    </a>
+                    <a className={styles.nextButton} onClick={showNextSlide}>
+                        &#10095;
+                    </a>
+                </div>
+            )}
             <div style={slideSize} className={styles.slider}>
-                {listSlides}
-            </div>
-        </div>
-    ) : (
-        <div style={slideSize} className={styles.container}>
-            <div className={styles.controller}>
-                <a className={styles.previousButton} onClick={showPreviousSlide}>
-                    &#10094;
-                </a>
-                <a className={styles.nextButton} onClick={showNextSlide}>
-                    &#10095;
-                </a>
-            </div>
-            <div style={slideSize} className={styles.slider}>
-                {listSlides}
+                {slides.map((slide, index, array) => (
+                    <div
+                        key={index}
+                        id={index}
+                        className={getSlideStatusStyles(index, array.length)}
+                        onClick={showSelectedSlide}
+                    >
+                        {slide}
+                    </div>
+                ))}
             </div>
         </div>
     );
