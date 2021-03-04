64edfab3262b85c2c1ae0a0bb3f29d31a5c92357
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-04 18:26:07 -0500

    added cycling-slider component

---
+++ components/ui-components/cycling-slider/cycling-slider.component.jsx
@@ -0,0 +1,15 @@
+/* React */
+import { useState, useEffect } from 'react';
+
+/* Utility */
+import PropType from 'prop-types';
+import { mergeStrings } from '../../../utils';
+
+/* Styles */
+import styles from './cycling-slider.module.scss';
+
+function CyclingSlider({ children }) {
+    return <div></div>;
+}
+
+export default CyclingSlider;
