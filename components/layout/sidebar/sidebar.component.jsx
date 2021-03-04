412a25874adc0f1af51b49aca2bce739aa8e46d2
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-04 01:39:27 -0500

    refactored sidebar effect

--- components/layout/sidebar/sidebar.component.jsx
+++ components/layout/sidebar/sidebar.component.jsx
@@ -1,20 +1,24 @@
+import { useState, useEffect, useRef } from 'react';
+import { useEventListener } from '../../hooks';
+
 import styles from './sidebar.module.scss';
 import utilStyles from '../../../styles/utils.module.scss';
-import { useState, useEffect, useRef } from 'react';
-import { useEventListner } from './hooks';
 
 function Sidebar() {
     const rootElementRef = useRef(null);
     const [clientHeight, setClientHeight] = useState(0);
     const [scrollHeight, setScrollHeight] = useState(0);
     const [scrollTop, setScrollTop] = useState(0);
+    useEventListener('scroll', handleScroll);
 
-    useEventListner('scroll', handleScroll);
-    // Get a reference for the root element
+    // Get the reference of the root element when a layout is mounted
     useEffect(() => {
         if (!rootElementRef.current) {
             rootElementRef.current = globalThis?.document?.documentElement;
         }
+        return () => {
+            rootElementRef.current = null;
+        };
     }, [rootElementRef.current]);
 
     // Whenever clientHeight or scrollHeight are changed, it call setter for them
@@ -24,18 +28,19 @@ function Sidebar() {
         setScrollTop(rootElementRef.current.scrollTop);
     }, [rootElementRef.current?.clientHeight, rootElementRef.current?.scrollHeight]);
 
-    function getGageStyle() {
-        const height =
-            scrollHeight === clientHeight ? 100 : Math.ceil((scrollTop / (scrollHeight - clientHeight)) * 100);
-        const style = { height: +height + '%' };
-        return style;
-    }
-    async function handleScroll() {
+    function handleScroll() {
         const newScrollTop = rootElementRef.current.scrollTop;
         // To prevent frequent Change, it only updates when the difference is bigger than 10
         if (newScrollTop > scrollTop + 20 || newScrollTop < scrollTop - 20) {
             setScrollTop(newScrollTop);
         }
+    }
+
+    function getGageStyle() {
+        const height =
+            scrollHeight === clientHeight ? 100 : Math.ceil((scrollTop / (scrollHeight - clientHeight)) * 100);
+        const style = { height: +height + '%' };
+        return style;
     }
 
     return (
