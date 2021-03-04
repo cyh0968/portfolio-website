be7f60a63d407137014cca4e709694246249a142
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-04 01:41:25 -0500

    imported new icons

--- components/ui-components/icons/index.js
+++ components/ui-components/icons/index.js
@@ -1,6 +1,8 @@
 export { IoLogoLinkedin as LinkedInIcon } from 'react-icons/io5';
 export { IoLogoGithub as GithubIcon } from 'react-icons/io5';
 export { IoLogoTwitter as TwitterIcon } from 'react-icons/io5';
+export { IoLogoMedium as MediumIcon } from 'react-icons/io5';
+export { IoDocumentTextOutline as DocumentIcon } from 'react-icons/io5';
 export { IoMailOutline as MailIcon } from 'react-icons/io5';
 export { IoDocumentOutline as ResumeIcon } from 'react-icons/io5';
 export { IoChevronDownSharp as ChevronDown } from 'react-icons/io5';
--- components/ui-components/index.js
+++ components/ui-components/index.js
@@ -2,6 +2,17 @@ export { Button } from './button';
 export { Cube } from './cube';
 export { FaceList } from './face-list';
 export { Ipad } from './ipad';
+export { ScrollAnimation } from './scroll-animation';
 export { Slider } from './slider';
 export { TypoAnimation } from './typo-animation';
-export { LinkedInIcon, GithubIcon, TwitterIcon, MailIcon, ResumeIcon, ChevronDown, ChevronUp } from './icons';
+export {
+    LinkedInIcon,
+    GithubIcon,
+    TwitterIcon,
+    MediumIcon,
+    DocumentIcon,
+    MailIcon,
+    ResumeIcon,
+    ChevronDown,
+    ChevronUp,
+} from './icons';
