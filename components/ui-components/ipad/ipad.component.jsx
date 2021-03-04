b1abda5d954736221afcbb0d565bce66e70003b4
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-04 01:39:05 -0500

    updated ipad component styles

--- components/ui-components/ipad/ipad.component.jsx
+++ components/ui-components/ipad/ipad.component.jsx
@@ -5,26 +5,13 @@ import utilStyles from '../../../styles/utils.module.scss';
 import { TypoAnimation } from '../typo-animation';
 
 const text = `
-import styles from './ipad.module.scss';
-import IpadSvg from '../../../assets/devices/ipad.svg';
-import utilStyles from '../../../styles/utils.module.scss';
+Thank you for visiting my website! I'm on the long journey of craftmanship. I'm a simple person
+who has only one purpose that is making good products.
 
-import { TypoAnimation } from '../typo-animation';
+I believe that the projects that I will work on will impact people's everyday lives, and I will
+be willing to take my responsibility to make the best.
 
-function Ipad() {
-  return (
-    <div className={styles.container}>
-        <IpadSvg className={styles.ipad}></IpadSvg>
-        <div className={styles.screen}>
-                <TypoAnimation>
-                    Hi!, I'm Yohan. Hello, World!
-                </TypoAnimation>
-            </div>
-        </div>
-    );
-}
-
-export default Ipad;
+A promise with myself is that I will remain humble and improve myself by the end of my journey.
 `;
 
 function Ipad() {
