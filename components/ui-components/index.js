4592c48daeda4e2cf31c275b13de2477d248fcf7
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-04 18:25:19 -0500

    exported new components

--- components/ui-components/index.js
+++ components/ui-components/index.js
@@ -1,8 +1,10 @@
 export { Button } from './button';
 export { Cube } from './cube';
+export { Date } from './date';
 export { FaceList } from './face-list';
 export { Ipad } from './ipad';
 export { ScrollAnimation } from './scroll-animation';
+export { Section } from './section';
 export { Slider } from './slider';
 export { TypoAnimation } from './typo-animation';
 export {
