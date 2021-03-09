4e939c4542e6bd673289cd4b6c0b5defb8d839f7
Yohan Choi <yohan.choi.dev@gmail.com>
2021-03-09 05:09:43 -0500

    updated 404 page styles and added link to home

--- pages/404/index.jsx
+++ pages/404/index.jsx
@@ -1,8 +1,27 @@
+/* Next */
+import Link from 'next/link';
+
+/* Styles */
+import styles from './index.module.scss';
+import utilStyles from '../../styles/utils.module.scss';
+
+/* Components */
 import { Layout } from '../../components/layout';
+import { Section } from '../../components/ui-components';
+
 function PageNotFound() {
   return (
     <Layout>
-      <h1>404 - Page Not Found</h1>
+      <div className={styles.container}>
+        <h1 className={utilStyles.heading2Xl}>404 Error - Page Not Found</h1>
+        <div className={styles.content}>
+          <h2 className={styles.goBackToHome}>
+            <Link href='/'>
+              <a className={utilStyles.link}>Go Back to Home</a>
+            </Link>
+          </h2>
+        </div>
+      </div>
     </Layout>
   );
 }
